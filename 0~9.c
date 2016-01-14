2f548b8c560095bb29969a97b36bf4b1c314e7de
mixi57 <mixi@mixideMacBook-Pro.local>
2016-01-15 01:37:41 +0800

    新的解法，效率提高

--- 0~9.c
+++ 0~9.c
@@ -1,20 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 #include "stdlib.h"
-int getBitNum(int data){
-	if(data == 0)
-		return 1;
-	int num = 0;
-	int bit = floor(data / 10);
-	while (bit != 0) {
-		num++;
-		bit = floor(bit / 10);
-	}
-	return num + 1;
-}
 
 int* getBitValue(int data){
-	int* gather = malloc(sizeof(int) * 10) ;
+	int* gather = malloc(sizeof(int) * 10);
 	memset(gather, -1, sizeof(int) * 10);
 
 	int index = 0;
@@ -25,32 +14,79 @@ int* getBitValue(int data){
 		bit = nextBit;
 		nextBit = floor(nextBit / 10);
 	}
-	//printf("bit = %d", bit);
+
 	gather[index++] = bit % 10;
-	gather[index++] = -1;
 	return gather;
 }
 int main(int argc, char *argv[]) {
 	srand((unsigned)time(NULL));
-	int value = rand()%10000 + 1;
-	printf("value %d", value);
+	int value = rand()%10000 + 1;//3401;//1234;//21;//99;//10;//123;//100;//1000;//3401;//
+	printf("value %d\n", value);	
 	
 	int valueGather[10];
 	memset(valueGather, 0, sizeof(int) * 10);
 	
-	for (int i =1; i <= value; i++) {
-		int* bitNumGather = getBitValue(i);
-		int index = 0;
-		int bitNum = bitNumGather[index];
-//		printf("\ni %d", i);
-		while (bitNum != -1) {
-//			printf(" %d ", bitNum);
-			valueGather[bitNum]++; 
-			bitNum = bitNumGather[++index];
+	int* bitValue = getBitValue(value);
+	//数字位数
+	int bit = 0;
+	while (bitValue[bit] >= 0) {
+		bit++;
+	}
+
+	// 把前面满的先算了
+	if (bit > 1) {
+		for (int i = 1; i < bit; i++) {
+			int zeroAddNum = i == 1 ? 0 : 9 * pow(10, i - 2) * (i - 1);
+			valueGather[0] += zeroAddNum;
+			for (int j = 1; j < 10; j++) {
+				valueGather[j] += zeroAddNum + pow(10, i - 1); 
+			}
+		}
+	} 
+
+	// 开始算后面的
+	const int num = bit;
+	int frontGather[num];
+	memset(frontGather, 0, sizeof(int) * num);
+	int frontIndex = 0;
+	while (bit > 1) {
+		// 
+		int v = bitValue[bit - 1];
+		int i = bit == num ? 1 : 0;
+
+		while (i < v) {
+			int otherAddNum = pow(10, bit - 2) * (bit - 1);
+			for (int index = 0; index <= 9; index++) {
+				valueGather[index] += otherAddNum + (i == index ? pow(10, bit - 1) : 0); 
+			}
+			i++;
+		}
+		if (frontIndex > 0 && v > 0) {
+			for (int i = 0; i < frontIndex; i++) {
+				valueGather[frontGather[i]] += (pow(10, bit -1) * v);
+			}
+		}
+		bit--;
+		frontGather[frontIndex++] = v;
+	}
+
+	//个位数
+	int v = bitValue[0];
+
+	for (int i = 1; i <= v; i++) {
+		valueGather[i] += 1;
+	}
+	if(num > 1)
+		valueGather[0] += 1;
+
+	if (frontIndex > 0) {
+		for (int i = 0; i < frontIndex; i++) {
+			valueGather[frontGather[i]] += (v + 1);
 		}
 	}
-	
+
 	for (int i = 0; i < 10; i++) {
 		printf("\n value %d num %d", i, valueGather[i]);
 	}
+	return 0;
 }
